2d664ca6d9b7c1284401e67382004d0ae3ffedf8
Mike Pearce <mike@m2infosec.com>
2025-02-07 15:07:38 +0000

    bah

--- run.py
+++ run.py
@@ -3,8 +3,7 @@ from app import create_app
 
 # Get config name from environment variable, default to 'development'
 config_name = os.environ.get('FLASK_ENV', 'development')
-application = create_app(config_name)
-app = application  # Add this line
+app = create_app(config_name)
 
 if __name__ == '__main__':
     app.run()
